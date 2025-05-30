703016834f4744e2a9879c0d7708868979e60fea
oladavis <oladavis425@gmail.com>
2025-05-30 05:35:52 -0700

    add html file

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Todo Item"
+        verbose_name_plural = "Todo Items"
+        ordering = ['completed', 'title']
